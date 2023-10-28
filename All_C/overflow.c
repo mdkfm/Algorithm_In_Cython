412ed3e930b0df44bd57d5c4b5c3d83424c73550
mdkfm <2119104071@qq.com>
2023-10-28 15:35:14 +0800

    bytes and overflow

--- All_C/overflow.c
+++ All_C/overflow.c
@@ -1,23 +1,117 @@
 #include<stdio.h>
 #include<limits.h>
+#include<stdlib.h>
 
+//***************** Overflow usually occurs during calculation *****************//
+/* There are some methods to check the calculation whether overflow */
+
+/* test the unsigned add */
 int uadd_ok(unsigned x, unsigned y){
     return (x + y) > x;
+}
+
+void test_uadd(){
+    unsigned a = 0x11111111;
+    unsigned b = 0x11111111;
+    printf("Max unsigned int: %u\n", UINT_MAX);  /* 32 bits */
+    printf("%d: %u + %u = %u\n", uadd_ok(a, b), a, b, a + b);
 }
 
 int tadd_ok(int x, int y){
     return !((x > 0 && y > 0 && x + y <= 0) || (x < 0 && y < 0 && x + y >= 0));
 }
 
-// int tadd_ok(int x, int y){
-//     int sum = x + y;
-//     return (sum - x == y) && (sum - y == x);
+int umult_ok(unsigned x, unsigned y){
+    unsigned p = x * y;
+    return !x || p / x == y;
+}
+
+int tmult_ok(int x, int y){
+    int p = x * y;
+    return !x || p / x == y;
+}
+
+/* We could use a more big int to receive the result */
+/* In fact, this is more readable and exact */
+// int tmult_ok(__int32_t x, __int32_t y){
+//     __int64_t result = x * (__int64_t) y;
+//     return (result <= INT_MAX) && (result >= INT_MIN);
 // }
+
+void test_tadd_tmult(){
+    int x = 0x7FFFFFFF;
+    int y = 0x80000000;
+    printf("Max int: %d, Min int: %d\n", INT_MAX, INT_MIN);  /* 32 bits */
+    printf("%d: %d + %d = %d\n", tadd_ok(x, y), x, y, x + y);
+    printf("%d: %d * %d = %d\n", tmult_ok(x, y), x, y, x * y);
+}
+
+
+//***************** The overflow also occurs when use unsigned *****************//
+
+float sum_elements(float a[], unsigned length){
+    int i;
+    float result = 0;
+
+    /* use i < length instead of i <= length */
+    // for(i = 0; i<= length - 1; i++) may overflow
+    for(i = 0; i < length; i++){
+        result += a[i];
+    }
+    return result;
+}
+
+/* size_t is unsigned */
+/* the second parameter of maaloc is size_t */
+/* We could check overflow before malloc */
+void *malloc_not_overflow(int ele_num, size_t ele_size){
+    u_int64_t required = ele_num * (u_int64_t) ele_size;
+    size_t request = (size_t) required;
+    if(request != required){
+        /* Overflow occurred */
+        return NULL;
+    }
+    void * result = malloc(request);
+    if(result == NULL){
+        /* malloc failed */
+        return NULL;
+    }
+    return result;
+}
+
+
+//***************** Float *****************//
+
+/* when we transform a big float to a int, which int could not denote */
+/* The Intel set it to be TMin */
+void test_float2int(){
+    float y = 1e400; /* overflow as inf*/
+    int x = y;
+    printf("Ifinity %f to int: %d\n", y, x);
+}
+
+/* The float32 could only denote 23 effective digits */
+/* The float64 is 52 */
+/* When we transfer a int with many effective digits to float */
+/* It may underflow */
+void test_int2float(){
+    int x = 987654321; /* int32*/
+    float y = x; /* float32*/
+    int z = y;
+    printf("int %d to float32 %.2f to int %d\n", x, y, z);
+
+    /* So, we should make sure the num of float digits big enough */
+    double b = x; /* float64 */
+    int c = b;
+    printf("int %d to float64 %.2lf to int %d\n", x, b, c);
+
+    long int a = 998877665544332211; /* int64 */
+    long double d = a; /* float128 */
+    long int e = d;
+    printf("int %ld to float64 %.2Lf to int %ld\n", a, d, e);
+}
 
 
 void main(){
-    int x = 0x7FFFFFFF;
-    int y = 0x7FFFFFFF;
-    printf("%d, %d\n", INT_MAX, INT_MIN);
-    printf("%d: %d + %d = %d\n", tadd_ok(x, y), x, y, x + y);
+    test_int2float();
 }
--- All_C/show_bytes.c
+++ All_C/show_bytes.c
@@ -3,6 +3,7 @@
 
 typedef unsigned char *byte_pointer;
 
+/* swap two value by swaping bytes */
 void bytes_swap(byte_pointer a, byte_pointer b, size_t len){
     size_t i;
     unsigned char tmp = 0;
@@ -13,6 +14,7 @@ void bytes_swap(byte_pointer a, byte_pointer b, size_t len){
     }
 }
 
+/* show the bytes of start with len */
 void show_bytes(byte_pointer start, size_t len){
     size_t i;
     for(i = 0; i < len; i++)
@@ -21,7 +23,7 @@ void show_bytes(byte_pointer start, size_t len){
     printf("\n");
 }
 
-
+/* the transform order is important*/
 void multitrans(){
     short ax = -12345;
     unsigned uy = ax; 
@@ -40,17 +42,43 @@ void multitrans(){
     show_bytes((byte_pointer) &uu, 4);
 }
 
-float sum_elements(float a[], unsigned length){
-    int i;
-    float result = 0;
 
-    for(i = 0; i < length; i++){
-        result += a[i];
-    }
-    return result;
+/* int divide by the power of 2, round to zero */
+/* when x >= 0, x >> k */
+/* when x < 0, (x + (1 << k) - 1) >> k */
+// int div_2expk(int x, unsigned k){
+//     return (x<0 ? x + (1 << k) - 1 : x) >> k;
+// }
+
+/* in fact, use the arithmetic shift right */
+int div_2expk(int x, unsigned k){
+    int bias = (x >> 31) & ((1 << k) - 1);
+    return (x + bias) >> k;
+}
+
+void test_div_2expk(){
+    int a = -53;
+    unsigned k = 2;
+    printf("div_2expk: %d/2^%d = %d, ", a, k, div_2expk(a, k));
+    printf("%d/2^%d = %d\n", -a, k, div_2expk(-a, k));
+    printf("Answer: %d, %d\n", a / (1 << k), (-a) / (1 << k));
+}
+
+
+/* in float, there are the special nums: POS_INFINITY, NEG_INFINITY, NEG_ZERO */
+#define POS_INFINITY 1e400 /* 1e400 positive overflow in float32 and float64 */
+#define NEG_INFINITY -POS_INFINITY
+#define NEG_ZERO (-1.0 / POS_INFINITY)
+void test_inifinity(){
+    double pinf = POS_INFINITY, ninf = NEG_INFINITY, nzero = NEG_ZERO;
+    printf("The positive infinity: %f, bytes: ",pinf);
+    show_bytes((byte_pointer) &pinf, sizeof(double));
+    printf("The negtive infinity: %f, bytes: ",ninf);
+    show_bytes((byte_pointer) &ninf, sizeof(double));
+    printf("The negtive zero:: %f, bytes: ",nzero);
+    show_bytes((byte_pointer) &nzero, sizeof(double));
 }
 
 void main(){
-    float a[] = {1, 2 ,3};
-    printf("%f", sum_elements(a, 3));
+    test_inifinity();
 }
